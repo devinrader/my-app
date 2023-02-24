6c252f82fdb311292bb085552ada8597881d8362
Devin Rader <devin@mindwhip.com>
2023-02-24 15:13:26 -0500

    fixing functions

--- functions/imageFromHtml.js
+++ functions/imageFromHtml.js
@@ -2,29 +2,48 @@ const Airtable = require('airtable');
 const https = require("https");
 const axios = require('axios');
 
-const base = new Airtable({apiKey: import.meta.env.VITE_AIRTABLE_KEY}).base('appls7XVxwxuDkhMg');
+const base = new Airtable({apiKey: process.env.VITE_AIRTABLE_KEY}).base('appls7XVxwxuDkhMg');
 
 exports.handler = async function(event, context) {
   var body = JSON.parse(event.body);
-
-  if(body != null && body.data != null) {
-    var data = body.data;
-    
+  if(body != null && body.payloads != null) {
+    var data = body.payloads[0].actionMetadata;
     //get the data from the webhook body payload
-    var email = payloads[0].actionMetadata.sourceMetadata.email
-
+    var email = data.sourceMetadata.user
+    console.log(email)
+    
     // Create an image by sending a POST to the API.
     // Retrieve your api_id and api_key from the Dashboard. https://htmlcsstoimage.com/dashboard
-    var imageurl = createImage(email.html);
+    const payload = { html: email.html };
+    
+    var imageUrl = ''
+    
+    let headers = { auth: {
+      username: process.env.VITE_HTMLTOIMAGE_USERID,
+      password: process.env.VITE_HTMLTOIMAGE_KEY
+    },
+    headers: {
+      'Content-Type': 'application/json'
+    }
+    }
+    try {
+      const response = await axios.post('https://hcti.io/v1/image', JSON.stringify(payload), headers);
+      console.log(response.data.url);
+      imageUrl = response.data.url;
+    } catch (error) {
+      console.error(error);
+    }
 
+  //console.log(imageUrl)
     //update the airtable record
-    base('Emails').update({
-      From: data.from,
-      Subject: data.subject,
-      Html: data.html,
-      Plain: data.text,
-      Headers: data.headers
-    }, err => {
+    base('Emails').update([
+      {
+        "id":email.id,
+        "fields": {
+          "RenderedImageUrl": imageUrl
+        }
+      }
+    ], err => {
       if (err) { console.error(err); return; }
       console.log('Message saved to Airtable')
       res.status(200).end();
@@ -37,22 +56,6 @@ exports.handler = async function(event, context) {
 
 
 async function createImage(html) {
-  const payload = { html: html };
-
-  let headers = { auth: {
-    username: import.meta.env.VITE_HTMLTOIMAGE_USERID,
-    password: import.meta.env.VITE_HTMLTOIMAGE_KEY
-  },
-  headers: {
-    'Content-Type': 'application/json'
-  }
-  }
-  try {
-    const response = await axios.post('https://hcti.io/v1/image', JSON.stringify(payload), headers);
-    console.log(response.data.url);
-    return response.data.url;
-  } catch (error) {
-    console.error(error);
-  }
+ 
 }
 
--- functions/receiveEmail.js
+++ functions/receiveEmail.js
@@ -1,27 +1,70 @@
-const Airtable = require('airtable');
 const https = require("https");
+const Airtable = require('airtable');
+const Busboy = require('busboy');
 
-const base = new Airtable({apiKey: import.meta.env.VITE_AIRTABLE_KEY}).base('appls7XVxwxuDkhMg');
+const base = new Airtable({apiKey: process.env.VITE_AIRTABLE_KEY}).base('appls7XVxwxuDkhMg');
 
 exports.handler = async function(event, context) {
-  var body = JSON.parse(event.body);
+  console.log(event.body)
 
-  if(body != null && body.data != null) {
-    var data = body.data;
-      
-    base('Emails').create({
-      From: data.from,
-      Subject: data.subject,
-      Html: data.html,
-      Plain: data.text,
-      Headers: data.headers
-    }, err => {
-      if (err) { console.error(err); return; }
-      console.log('Message saved to Airtable')
-      res.status(200).end();
-    })
-  }
+  const fields = await parseMultipartForm(event)
+
+  //console.log(fields)
+  //if(body != null && body.data != null) {
+  //  var data = body.data;
+  //  console.log(data)    
+  //   base('Emails').create({
+  //     From: data.from,
+  //     Subject: data.subject,
+  //     Html: data.html,
+  //     Plain: data.text,
+  //     Headers: data.headers
+  //   }, err => {
+  //     if (err) { console.error(err); return; }
+  //     console.log('Message saved to Airtable')
+  //     res.status(200).end();
+  //   })
+  //}
   return {
       statusCode: 200
   }
+}
+
+// https://answers.netlify.com/t/trouble-with-handling-files-in-netlify-function/42418/34
+function parseMultipartForm(event) {
+  return new Promise((resolve) => {
+    console.log("Starting parse")
+    // we'll store all form fields inside of this
+    const fields = {};
+
+    const busboy = Busboy({
+      headers: event.headers
+    });
+
+    busboy.on("file", (fieldname, filestream, fileinfo) => {
+      const { filename, transferEncoding, mimeType } = info
+      filestream.on("data", (data) => {
+        if (!fields[fieldname]) {
+          fields[fieldname] = []
+        }
+
+        fields[fieldname].push({
+          filename,
+          type: mimeType,
+          content: data,
+        });
+      });
+    });
+
+    busboy.on("field", (fieldName, value) => {
+      console.log(`Field Found ${fieldName}`)
+      fields[fieldName] = value;
+    });
+
+    busboy.on("close", () => {
+      resolve(fields)
+    });
+
+    busboy.end(Buffer.from(event.body, 'base64'))
+  });
 }
