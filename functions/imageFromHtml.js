baf32a78b29623fa4f27e39028a213e41fe1bd2c
Devin Rader <devin@mindwhip.com>
2023-02-27 21:50:53 -0500

    fixing html 2 img

--- functions/imageFromHtml.js
+++ functions/imageFromHtml.js
@@ -6,18 +6,20 @@ const base = new Airtable({apiKey: process.env.VITE_AIRTABLE_KEY}).base('appls7X
 
 exports.handler = async function(event, context) {
   var body = JSON.parse(event.body);
-  if(body != null && body.payloads != null) {
-    var data = body.payloads[0].actionMetadata;
-    //get the data from the webhook body payload
-    var email = data.sourceMetadata.user
-    console.log(email)
-    
+  if(body != null && body.payloads != null && body.payloads.length > 0) {
+    var payloads = body.payloads
+
+    var changedTable = payloads[0].changedTablesById
+    var createdRecord = Object.values(changedTable)[0].createdRecordsById
+    console.log(changedTable)
+    console.log(createdRecord)
+
     // Create an image by sending a POST to the API.
     // Retrieve your api_id and api_key from the Dashboard. https://htmlcsstoimage.com/dashboard
-    const payload = { html: email.html };
-    
+    const htmlemail = { html: createdRecord.cellValuesByFieldId.Html };
+
     var imageUrl = ''
-    
+
     let headers = { auth: {
       username: process.env.VITE_HTMLTOIMAGE_USERID,
       password: process.env.VITE_HTMLTOIMAGE_KEY
@@ -27,18 +29,18 @@ exports.handler = async function(event, context) {
     }
     }
     try {
-      const response = await axios.post('https://hcti.io/v1/image', JSON.stringify(payload), headers);
+      const response = await axios.post('https://hcti.io/v1/image', JSON.stringify(htmlemail), headers);
       console.log(response.data.url);
       imageUrl = response.data.url;
     } catch (error) {
       console.error(error);
     }
 
-  //console.log(imageUrl)
+    console.log(imageUrl)
     //update the airtable record
     base('Emails').update([
       {
-        "id":email.id,
+        "id":object.values(createdRecord)[0],
         "fields": {
           "RenderedImageUrl": imageUrl
         }
@@ -55,7 +57,7 @@ exports.handler = async function(event, context) {
 }
 
 
-async function createImage(html) {
- 
-}
+// async function createImage(html) {
+
+// }
 
